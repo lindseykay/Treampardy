3d0fef6027157a96eb7728870bac58e858f28502
jwpettit <jwpettit@me.com>
2022-11-01 15:51:44 -0700

    Made it work to pull data for the game

--- src/GameComponents/GameBoard.jsx
+++ src/GameComponents/GameBoard.jsx
@@ -12,12 +12,12 @@ export default function GameBoard() {
     }
     const [url1, url2, url3, url4, url5, url6] = urlNumsList;
 
-    const [category3, setCategory3] = useState([])
     const [category1, setCategory1] = useState([])
+    const [category2, setCategory2] = useState([])
+    const [category3, setCategory3] = useState([])
     const [category4, setCategory4] = useState([])
     const [category5, setCategory5] = useState([])
     const [category6, setCategory6] = useState([])
-    const [category2, setCategory2] = useState([])
 
 
 
@@ -25,7 +25,6 @@ export default function GameBoard() {
         async function fetchCategory1() {
             const url = `http://jservice.io/api/category?id=${url1}`;
             const response = await fetch(url);
-
             if (response.ok) {
                 const data = await response.json();
                 setCategory1(data);
@@ -38,7 +37,6 @@ export default function GameBoard() {
         async function fetchCategory2() {
             const url = `http://jservice.io/api/category?id=${url2}`;
             const response = await fetch(url);
-
             if (response.ok) {
                 const data = await response.json();
                 setCategory2(data);
@@ -51,7 +49,6 @@ export default function GameBoard() {
         async function fetchCategory3() {
             const url = `http://jservice.io/api/category?id=${url3}`;
             const response = await fetch(url);
-
             if (response.ok) {
                 const data = await response.json();
                 setCategory3(data);
@@ -64,7 +61,6 @@ export default function GameBoard() {
         async function fetchCategory4() {
             const url = `http://jservice.io/api/category?id=${url4}`;
             const response = await fetch(url);
-
             if (response.ok) {
                 const data = await response.json();
                 setCategory4(data);
@@ -77,7 +73,6 @@ export default function GameBoard() {
         async function fetchCategory5() {
             const url = `http://jservice.io/api/category?id=${url5}`;
             const response = await fetch(url);
-
             if (response.ok) {
                 const data = await response.json();
                 setCategory5(data);
@@ -90,7 +85,6 @@ export default function GameBoard() {
         async function fetchCategory6() {
             const url = `http://jservice.io/api/category?id=${url6}`;
             const response = await fetch(url);
-
             if (response.ok) {
                 const data = await response.json();
                 setCategory6(data);
@@ -108,12 +102,12 @@ export default function GameBoard() {
     return (
         <>
             <div>
-                <p>{category1}</p>
-                <p>{category2}</p>
+                <p>{category1.title}</p>
+                {/* <p>{category2}</p>
                 <p>{category3}</p>
                 <p>{category4}</p>
                 <p>{category5}</p>
-                <p>{category6}</p>
+                <p>{category6}</p> */}
             </div>
         </>
     )

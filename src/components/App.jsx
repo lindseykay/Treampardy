af7ebaaa5c8fe19320d4f2b2246cb30ea94d42d7
Jeff Pettit <jwpettit@me.com>
2022-12-15 02:44:28 +0000

    Added {} around prop import of basename

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Game from './Game';
 import { BrowserRouter } from 'react-router-dom';
 
-export default function App(basename) {
+export default function App({basename}) {
   return (
     <BrowserRouter basename={basename}>
       <Game/>

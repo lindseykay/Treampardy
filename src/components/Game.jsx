89bae4e936ab4d69c9ec07e281d0ef3e389effe0
jwpettit <jwpettit@me.com>
2022-12-16 14:18:58 -0800

    Tried refactoring game.jsx to use a for loop to fetch categories

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,83 +1,125 @@
 import React, { useState, useEffect } from "react"
 import { Routes, Route, useNavigate } from 'react-router-dom';
-
 import GameBoard from "../GameComponents/GameBoard";
 import EndPage from "../GameComponents/EndPage";
 import StartPage from "../GameComponents/StartPage";
 
-let urlNumsList = [];
-while (urlNumsList.length < 6) {
-    let urlNum = Math.floor(Math.random() * 27723 + 1);
-    if (!urlNumsList.includes(urlNum)) {
-        urlNumsList.push(urlNum);
-    }
-}
-const [url1, url2, url3, url4, url5, url6] = urlNumsList;
+// let urlNumsList = [];
+// while (urlNumsList.length < 6) {
+//     let urlNum = Math.floor(Math.random() * 27723 + 1);
+//     if (!urlNumsList.includes(urlNum)) {
+//         urlNumsList.push(urlNum);
+//     }
+// }
+// const [url1, url2, url3, url4, url5, url6] = urlNumsList;
 
 export default function Game() {
 
-    const [category1, setCategory1] = useState({})
-    const [category2, setCategory2] = useState({})
-    const [category3, setCategory3] = useState({})
-    const [category4, setCategory4] = useState({})
-    const [category5, setCategory5] = useState({})
-    const [category6, setCategory6] = useState({})
-    const [selectedClue, setSelectedClue] = useState({})
-    const [showClueBool, setShowClueBool] = useState(false)
-    const [showAnswerBool, setShowAnswerBool] = useState(false)
-    const [selectedCategory, setSelectedCategory] = useState({})
-    const [selectedValue, setSelectedValue] = useState(0)
-    const [score, setScore] = useState(0)
+    const [category1, setCategory1] = useState({});
+    const [category2, setCategory2] = useState({});
+    const [category3, setCategory3] = useState({});
+    const [category4, setCategory4] = useState({});
+    const [category5, setCategory5] = useState({});
+    const [category6, setCategory6] = useState({});
+    const [selectedClue, setSelectedClue] = useState({});
+    const [showClueBool, setShowClueBool] = useState(false);
+    const [showAnswerBool, setShowAnswerBool] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState({});
+    const [selectedValue, setSelectedValue] = useState(0);
+    const [score, setScore] = useState(0);
 
     let navigate = useNavigate();
 
-    const fetchCategories = async() => {
-        const fullUrl1 = `https://jservice.io/api/category?id=${url1}`;
-        const fullUrl2 = `https://jservice.io/api/category?id=${url2}`;
-        const fullUrl3 = `https://jservice.io/api/category?id=${url3}`;
-        const fullUrl4 = `https://jservice.io/api/category?id=${url4}`;
-        const fullUrl5 = `https://jservice.io/api/category?id=${url5}`;
-        const fullUrl6 = `https://jservice.io/api/category?id=${url6}`;
-        const fetchParameters = {
-            method: 'GET',
-            mode: 'cors',
-            credentials: 'same-origin',
-            // referrerPolicy: 'strict-origin-when-cross-origin'
-            // headers: {
-            //     // 'origin': '',
-            // },
+    const functionList = [setCategory1(), setCategory2(), setCategory3(), setCategory4(), setCategory5(), setCategory6()];
+
+    // useEffect(() => {
+    //     const fetchCategories = async() => {
+    //     for (let i = 0; i < urlNumsList.length; i++) {
+    //         const response = await fetch (`https://jservice.io/api/category?id=${urlNumsList[i]}`)
+    //         if (response.ok) {
+    //             const data = await response.json();
+    //             console.log(data);
+    //             functionList[i](data);
+    //         } else {
+    //             console.log("PROBLEMS!");
+    //         };
+    //     };}
+    //     fetchCategories();
+    //     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, []);
+
+    let urlNumsList = [];
+
+    const generateRandomNumbers = () => {
+        while (urlNumsList.length < 6) {
+            let urlNum = Math.floor(Math.random() * 27723 + 1);
+            if (!urlNumsList.includes(urlNum)) {
+                urlNumsList.push(urlNum);
+            };
         };
-        const response1 = await fetch(fullUrl1, fetchParameters);
-        const response2 = await fetch(fullUrl2, fetchParameters);
-        const response3 = await fetch(fullUrl3, fetchParameters);
-        const response4 = await fetch(fullUrl4, fetchParameters);
-        const response5 = await fetch(fullUrl5, fetchParameters);
-        const response6 = await fetch(fullUrl6, fetchParameters);
-        if (
-            response1.ok &&
-            response2.ok &&
-            response3.ok &&
-            response4.ok &&
-            response5.ok &&
-            response6.ok
-        ) {
-            const data1 = await response1.json();
-            const data2 = await response2.json();
-            const data3 = await response3.json();
-            const data4 = await response4.json();
-            const data5 = await response5.json();
-            const data6 = await response6.json();
-            setCategory1(data1);
-            setCategory2(data2);
-            setCategory3(data3);
-            setCategory4(data4);
-            setCategory5(data5);
-            setCategory6(data6);
-        }
     };
 
+    const fetchCategories = async() => {
+    for (let i = 0; i < urlNumsList.length; i++) {
+        const response = await fetch (`https://jservice.io/api/category?id=${urlNumsList[i]}`)
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            functionList[i](data);
+        } else {
+            console.log("PROBLEMS!");
+        };
+    };}
+
+    // const fetchCategories = async() => {
+    //     const fullUrl1 = `http://jservice.io/api/category?id=${url1}`;
+    //     const fullUrl2 = `http://jservice.io/api/category?id=${url2}`;
+    //     const fullUrl3 = `http://jservice.io/api/category?id=${url3}`;
+    //     const fullUrl4 = `http://jservice.io/api/category?id=${url4}`;
+    //     const fullUrl5 = `http://jservice.io/api/category?id=${url5}`;
+    //     const fullUrl6 = `http://jservice.io/api/category?id=${url6}`;
+    //     const fetchParameters = {
+    //         // method: 'GET',
+    //         // mode: 'cors',
+    //         // credentials: 'same-origin',
+    //         // // referrerPolicy: 'strict-origin-when-cross-origin'
+    //         // // headers: {
+    //         // //     // 'origin': '',
+    //         // // },
+    //     };
+    //     const response1 = await fetch(fullUrl1, fetchParameters);
+    //     const response2 = await fetch(fullUrl2, fetchParameters);
+    //     const response3 = await fetch(fullUrl3, fetchParameters);
+    //     const response4 = await fetch(fullUrl4, fetchParameters);
+    //     const response5 = await fetch(fullUrl5, fetchParameters);
+    //     const response6 = await fetch(fullUrl6, fetchParameters);
+    //     if (
+    //         response1.ok &&
+    //         response2.ok &&
+    //         response3.ok &&
+    //         response4.ok &&
+    //         response5.ok &&
+    //         response6.ok
+    //     ) {
+    //         const data1 = await response1.json();
+    //         const data2 = await response2.json();
+    //         const data3 = await response3.json();
+    //         const data4 = await response4.json();
+    //         const data5 = await response5.json();
+    //         const data6 = await response6.json();
+    //         setCategory1(data1);
+    //         setCategory2(data2);
+    //         setCategory3(data3);
+    //         setCategory4(data4);
+    //         setCategory5(data5);
+    //         setCategory6(data6);
+    //     }
+    // };
+
     useEffect(() => {
-        fetchCategories()
+        generateRandomNumbers();
+        fetchCategories();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     function selectClue(category, clueNumber) {
